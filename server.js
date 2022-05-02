31fc7d9fa4ff40c757e562e358b9ecc2b7267b1b
Brayan Santos <santos.a.brayan97@gmail.com>
2022-05-02 10:39:33 -0400

    components,pages,router files created - mongoose db connected

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI;
+const db = mongoose.connection;
 
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+db.on('open', () => {
+    console.log('Mongo is Connected');
+});
+
+app.use(express.json());
 
 
 
 app.listen(PORT,()=>{
-    console.log('WE OUTSIDE', PORT);
+    console.log('Server Connected', PORT);
 });
